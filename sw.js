4d5092cae13350f978955d3f2d95099e346f9855
SATHIRI-SATMEC <jorgeluisleiva2020@gmail.com>
2026-07-09 13:17:02 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,9 @@
-const CACHE='avance-obra-v4';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
-  );
+const CACHE='avance-obra-v5';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
-// Siempre va a la red primero, sin caché
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
